f0001452609f2d61f27700113f39c41f1c9bec77
yaroslav <zyzy92187@gmail.com>
2023-02-12 13:22:47 +0200

    change github signin button

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -35,7 +35,7 @@ const AuthWithGoogle = ({ provider }: ProviderAuthProps) => {
   return (
     <button className="text-white bg-blue-700 border border-transparent hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 disabled:hover:bg-blue-700 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:disabled:hover:bg-blue-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-center font-medium focus:z-10 rounded-lg w-full mt-5 bg-gray-900" onClick={() => signIn(provider.id)}>
       <span className="flex items-center rounded-md text-sm px-4 py-2">
-        <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" x="0px" y="0px" viewBox="0 0 48 48" enable-background="new 0 0 48 48" className="mx-2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+        <svg stroke="currentColor" fill="currentColor" strokeWidth="0" version="1.1" x="0px" y="0px" viewBox="0 0 48 48" enableBackground="new 0 0 48 48" className="mx-2" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
           <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
 	c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
 	c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
@@ -54,9 +54,9 @@ const AuthWithGoogle = ({ provider }: ProviderAuthProps) => {
 
 const AuthWithGitHub = ({ provider }: ProviderAuthProps) => {
   return (
-    <button className="text-white bg-blue-700 border border-transparent hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 disabled:hover:bg-blue-700 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:disabled:hover:bg-blue-600 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-center font-medium focus:z-10 rounded-lg w-full mt-5 bg-gray-900" onClick={() => signIn(provider.id)}>
+    <button className="text-white bg-black border border-transparent hover:bg-gray-900 focus:ring-4 focus:ring-blue-300 disabled:hover:bg-blue-700 dark:bg-black dark:hover:bg-gray-900 dark:focus:ring-gray-900 dark:disabled:hover:ring-gray-700 focus:!ring-2 group flex h-min items-center justify-center p-0.5 text-center font-medium focus:z-10 rounded-lg w-full mt-5 bg-gray-900" onClick={() => signIn(provider.id)}>
       <span className="flex items-center rounded-md text-sm px-4 py-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg>
+      <svg className="dark:bg-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0v24h24v-24h-24zm14.534 19.59c-.406.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.311-1.588-.824-2.147.083-.202.357-1.016-.079-2.117 0 0-.671-.215-2.198.82-.639-.18-1.323-.267-2.003-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>
         Sign in with GitHub
       </span>
     </button>
@@ -70,20 +70,6 @@ export default function SignIn({ providers, csrfToken }: InferGetServerSideProps
   return (
     <Layout>
       <div className="max-w-sm w-full rounded-lg shadow dark:bg-transparent dark:border-gray-700">
-        {/* {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            {
-              (() => {
-                switch (provider.name) {
-                  case "Email": return <EmailForm csrfToken={csrfToken} />
-                  case "Google": return <AuthWithGoogle provider={provider} />
-                  case "GitHub": return <AuthWithGitHub provider={provider} />
-                }
-              })()
-            }
-
-          </div>
-        ))} */}
         <EmailForm csrfToken={csrfToken} />
         <AuthWithGoogle provider={providersValues.find(provider => provider.name === "Google")} />
         <AuthWithGitHub provider={providersValues.find(provider=>provider.name === "GitHub")}/>
